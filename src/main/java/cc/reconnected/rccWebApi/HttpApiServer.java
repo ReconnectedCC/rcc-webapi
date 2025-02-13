26078721ff1a1c069aee079c33df895e103248d3
Alessandro Proto <alex@alexdevs.me>
2025-02-13 23:00:37 +0100

    Bugfix the entire mod not actually starting the HTTP server

--- src/main/java/cc/reconnected/rccWebApi/HttpApiServer.java
+++ src/main/java/cc/reconnected/rccWebApi/HttpApiServer.java
@@ -6,6 +6,7 @@ import com.sun.net.httpserver.HttpServer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.MinecraftServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -31,7 +32,11 @@ public class HttpApiServer {
             RccWebApi.LOGGER.error("Could not start HTTP API server", e);
             return;
         }
-        new Thread(() -> instance.httpServer().start());
+
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            RccWebApi.LOGGER.info("Starting HTTP API server on {}", instance.httpServer().getAddress().toString());
+            instance.httpServer().start();
+        });
 
         ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
             RccWebApi.LOGGER.info("Stopping HTTP services");
@@ -45,12 +50,14 @@ public class HttpApiServer {
             }
         });
 
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            currentPlayerCount = server.getCurrentPlayerCount() + 1;
-        });
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> instance.updatePlayerCount(server));
 
-        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
-            currentPlayerCount = server.getCurrentPlayerCount() - 1;
+        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> instance.updatePlayerCount(server));
+    }
+
+    private void updatePlayerCount(final MinecraftServer server) {
+        RccWebApi.schedule(() -> {
+            currentPlayerCount = server.getCurrentPlayerCount();
         });
     }
 
@@ -64,7 +71,7 @@ public class HttpApiServer {
         server = HttpServer.create(new InetSocketAddress(RccWebApi.CONFIG.httpPort), 0);
         server.createContext("/tps", new TPSHandler());
         server.createContext("/mspt", new MSPTHandler());
-        server.createContext("/player", new PlayerCountHandler());
+        server.createContext("/players", new PlayerCountHandler());
         server.setExecutor(null);
     }
 
@@ -82,10 +89,10 @@ public class HttpApiServer {
     static class MSPTHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            var tps = String.valueOf(currentTps);
-            t.sendResponseHeaders(200, tps.length());
+            var mspt = String.valueOf(currentMspt);
+            t.sendResponseHeaders(200, mspt.length());
             var body = t.getResponseBody();
-            body.write(tps.getBytes());
+            body.write(mspt.getBytes());
             body.close();
         }
     }
@@ -93,10 +100,10 @@ public class HttpApiServer {
     static class PlayerCountHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            var tps = String.valueOf(currentPlayerCount);
-            t.sendResponseHeaders(200, tps.length());
+            var count = String.valueOf(currentPlayerCount);
+            t.sendResponseHeaders(200, count.length());
             var body = t.getResponseBody();
-            body.write(tps.getBytes());
+            body.write(count.getBytes());
             body.close();
         }
     }
--- src/main/java/cc/reconnected/rccWebApi/RccWebApi.java
+++ src/main/java/cc/reconnected/rccWebApi/RccWebApi.java
@@ -1,17 +1,20 @@
 package cc.reconnected.rccWebApi;
 
-import cc.reconnected.library.RccLibConfig;
 import cc.reconnected.library.config.ConfigManager;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RccWebApi implements ModInitializer {
     public static final String MOD_ID = "rcc-webapi";
     public static RccWebApiConfig CONFIG;
     public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private static final ConcurrentLinkedQueue<Runnable> tickQueue = new ConcurrentLinkedQueue<>();
 
     @Override
     public void onInitialize() {
@@ -22,9 +25,18 @@ public class RccWebApi implements ModInitializer {
         }
 
         HttpApiServer.register();
+
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            tickQueue.forEach(Runnable::run);
+            tickQueue.clear();
+        });
     }
 
     public static void loadConfig() throws IOException {
         CONFIG = ConfigManager.load(RccWebApiConfig.class);
+    }
+
+    public static void schedule(Runnable runnable) {
+        tickQueue.add(runnable);
     }
 }
